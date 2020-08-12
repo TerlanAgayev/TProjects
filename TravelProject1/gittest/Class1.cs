5f60ffb07d9b93cc1cf60ff8fcea6025e0e4cb24
Terlan Agayev <terlan.agayev.97@mail.ru>
2020-08-12 21:04:25 +0400

    Version 1.1 f

---
+++ TravelProject1/gittest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelProject1.gittest
+{
+    public class Class1
+    {
+        public int cem()
+        {
+            int a = 5;
+            int b = 4;
+            int c = a + b;
+            return c;
+        }
+    }
+}
